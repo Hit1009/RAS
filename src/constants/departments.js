2fb7ab144858b477732c634ec8b65f4403c0fe38
Hit1009 <hitarthvyas234@gmail.com>
2024-02-23 00:28:00 +0530

    updated info, responsiveness improved

---
+++ src/constants/departments.js
@@ -0,0 +1,25 @@
+
+const data = [
+    {
+        id: 1,
+        icon: "assets/members/image1.jpeg",
+        name: "Web-Dev/App-Dev",
+    },
+    {
+        id: 2,
+        icon: "assets/members/image1.jpeg",
+        name: "Design",
+    },
+    {
+        id: 3,
+        icon: "assets/members/image1.jpeg",
+        name: "Projects",
+    },
+    {
+        id: 4,
+        icon: "assets/members/image1.jpeg",
+        name: "Operations",
+    },
+];
+
+export default data;
